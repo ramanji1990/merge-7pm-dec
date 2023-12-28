be41ba0f3e74f401d7a108ff081a1463d1739ab2
RamanjiReddyvempalli <ramanji.devops@gmail.com>
2023-12-28 19:38:26 +0530

    adding provider.tf file

---
+++ provider.tf
@@ -0,0 +1,9 @@
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+  environment     = "public"
+  subscription_id = "a9e475f7-cea0-4ea4-a467-1a549bcbb641"
+  client_id = "c6b42a2e-011f-48a1-a648-32ab26316fd3"
+  client_secret = "QDp8Q~wh8aS~qF.CrJ5IRqMkSC~fhwlZoMXJrdBo"
+  tenant_id = "a851a3d0-698b-4bf0-a389-dd1cc6cde562"
+}
